1a33d0e6ce3a905cfa69d58a39012fbce1d5929e
CL4275 <caioemariana2004@gmail.com>
2025-05-18 13:43:17 -0300

    Versão final estilizada

--- models/alimento.js
+++ models/alimento.js
@@ -40,20 +40,9 @@ const Alimento = sequelize.define('Alimento', {
     type: DataTypes.STRING,
     allowNull: true,
   },
-  // --- NOVOS CAMPOS DE GEOLOCALIZAÇÃO ---
   address: {
     type: DataTypes.STRING, // Para o endereço de texto legível
     allowNull: true, // Endereço pode ser opcional
-  },
-  latitude: {
-    type: DataTypes.DECIMAL(10, 8), // DECIMAL com precisão para latitude
-    allowNull: true, // Latitude pode ser opcional
-    // Você pode adicionar validação para verificar o range de latitude (-90 a +90)
-  },
-  longitude: {
-    type: DataTypes.DECIMAL(11, 8), // DECIMAL com precisão para longitude
-    allowNull: true, // Longitude pode ser opcional
-    // Você pode adicionar validação para verificar o range de longitude (-180 a +180)
   },
 });
 
